6894ca3be566caa0141bd5b1a94bf950c6e39a86
sd123-sd <sdas31562@gmail.com>
2024-07-10 16:01:56 +0530

    initial

---
+++ src/Components/UserRole.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../assets/UserRole.css"
+
+  const Role = () =>  {
+	return (
+    <div className="userid">
+      <h2>User Role</h2>
+      <div className="roleForm">
+        <form action="/submit" method="post">
+          <label htmlFor="username">User Name</label>
+          <input placeholder="username" type="text" className="username" required />
+          <label htmlFor="userrole">Role</label>
+          <select id="userrole" required defaultValue="">
+            <option value="" disabled>Select</option>
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+            <option value="admin">Admin</option>
+          </select>
+          <input className="submitbtn_role" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+    );
+}
+ 
+export default Role;
